58d6a532b017cb0a87fdef753013c293298d3d91
pritikharkar <101193553+pritikharkar@users.noreply.github.com>
2022-05-10 13:45:46 +0530

    Add files via upload

---
+++ GSprimeAcc.java
@@ -0,0 +1,19 @@
+package shopFactoryPac;
+
+public class GSPrimeAcc {
+
+private static final float charges = 0;
+
+public void GSPrimeAcc() {
+
+}
+
+public void bookProduct() {
+float f4 = 0f;
+}
+
+public String toString() {
+String f5 = null;
+return f5;
+}
+}
