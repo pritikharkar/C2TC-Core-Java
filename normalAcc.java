3fbe690e75e697dc1d602ee45e839123601767a5
pritikharkar <101193553+pritikharkar@users.noreply.github.com>
2022-05-10 13:45:01 +0530

    Add files via upload

---
+++ normalAcc.java
@@ -0,0 +1,20 @@
+package shopFactoryPac;
+
+public abstract class normalAcc extends shopAcc {
+
+private final float deliveryCharges = 0;
+
+public void normalAcc() {
+
+}
+
+public void bookProduct() {
+float f2 = 0f;
+}
+
+public String toString() {
+String f3 = null;
+return f3;
+}
+
+}
