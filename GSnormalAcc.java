761ef79ce8955958966718fd8020da2f426ea5b4
pritikharkar <101193553+pritikharkar@users.noreply.github.com>
2022-05-10 13:46:13 +0530

    Add files via upload

---
+++ GSnormalAcc.java
@@ -0,0 +1,19 @@
+package shopFactoryPac;
+
+public class GSNormalAcc {
+
+public void GSNormalAcc() {
+
+}
+
+public void bookProduct() {
+float f5 = 0f;
+}
+
+public String toString() {
+String f6 = null;
+return f6;
+}
+
+
+}
